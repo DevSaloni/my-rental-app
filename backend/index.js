5932f03bd069edc296d70453b8c0a3b3b0c968b5
DevSaloni <168973005+DevSaloni@users.noreply.github.com>
2025-08-04 15:49:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ app.use('/api', contactRoute);
 app.use('/api/properties', propertyRoute);
 
 const PORT = process.env.PORT || 2010;
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
